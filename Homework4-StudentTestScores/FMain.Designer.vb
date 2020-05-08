7c62b0c6e5df2ac14677845e87f8d46ac79a7b5d
shahraj1 <shahraj603@gmail.com>
2020-05-08 13:35:13 -0400

    first commit

---
+++ Homework4-StudentTestScores/FMain.Designer.vb
@@ -0,0 +1,354 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnCalculateAverage = New System.Windows.Forms.Button()
+        Me.txtTest3c = New System.Windows.Forms.TextBox()
+        Me.txtAverage3 = New System.Windows.Forms.TextBox()
+        Me.txtAverage2 = New System.Windows.Forms.TextBox()
+        Me.txtTest2c = New System.Windows.Forms.TextBox()
+        Me.txtTest1c = New System.Windows.Forms.TextBox()
+        Me.txtTest3a = New System.Windows.Forms.TextBox()
+        Me.txtTest3b = New System.Windows.Forms.TextBox()
+        Me.txtTest2a = New System.Windows.Forms.TextBox()
+        Me.txtTest2b = New System.Windows.Forms.TextBox()
+        Me.txtAverage1 = New System.Windows.Forms.TextBox()
+        Me.txtTest1b = New System.Windows.Forms.TextBox()
+        Me.txtTest1a = New System.Windows.Forms.TextBox()
+        Me.txtName3 = New System.Windows.Forms.TextBox()
+        Me.txtName2 = New System.Windows.Forms.TextBox()
+        Me.txtName1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.mnuMenu = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.msReload = New System.Windows.Forms.ToolStripMenuItem()
+        Me.msSave = New System.Windows.Forms.ToolStripMenuItem()
+        Me.msExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuReport = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuAbout = New System.Windows.Forms.ToolStripMenuItem()
+        Me.msCalculateAverage = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.mnuMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnExit)
+        Me.GroupBox1.Controls.Add(Me.btnClear)
+        Me.GroupBox1.Controls.Add(Me.btnCalculateAverage)
+        Me.GroupBox1.Controls.Add(Me.txtTest3c)
+        Me.GroupBox1.Controls.Add(Me.txtAverage3)
+        Me.GroupBox1.Controls.Add(Me.txtAverage2)
+        Me.GroupBox1.Controls.Add(Me.txtTest2c)
+        Me.GroupBox1.Controls.Add(Me.txtTest1c)
+        Me.GroupBox1.Controls.Add(Me.txtTest3a)
+        Me.GroupBox1.Controls.Add(Me.txtTest3b)
+        Me.GroupBox1.Controls.Add(Me.txtTest2a)
+        Me.GroupBox1.Controls.Add(Me.txtTest2b)
+        Me.GroupBox1.Controls.Add(Me.txtAverage1)
+        Me.GroupBox1.Controls.Add(Me.txtTest1b)
+        Me.GroupBox1.Controls.Add(Me.txtTest1a)
+        Me.GroupBox1.Controls.Add(Me.txtName3)
+        Me.GroupBox1.Controls.Add(Me.txtName2)
+        Me.GroupBox1.Controls.Add(Me.txtName1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(28, 41)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(522, 321)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Student Grade Report"
+        '
+        'btnCalculateAverage
+        '
+        Me.btnCalculateAverage.Location = New System.Drawing.Point(65, 244)
+        Me.btnCalculateAverage.Name = "btnCalculateAverage"
+        Me.btnCalculateAverage.Size = New System.Drawing.Size(122, 49)
+        Me.btnCalculateAverage.TabIndex = 18
+        Me.btnCalculateAverage.Text = "Calculate Average"
+        Me.btnCalculateAverage.UseVisualStyleBackColor = True
+        '
+        'txtTest3c
+        '
+        Me.txtTest3c.Location = New System.Drawing.Point(331, 192)
+        Me.txtTest3c.Name = "txtTest3c"
+        Me.txtTest3c.Size = New System.Drawing.Size(60, 22)
+        Me.txtTest3c.TabIndex = 16
+        '
+        'txtAverage3
+        '
+        Me.txtAverage3.Location = New System.Drawing.Point(432, 192)
+        Me.txtAverage3.Name = "txtAverage3"
+        Me.txtAverage3.Size = New System.Drawing.Size(60, 22)
+        Me.txtAverage3.TabIndex = 17
+        '
+        'txtAverage2
+        '
+        Me.txtAverage2.Location = New System.Drawing.Point(432, 149)
+        Me.txtAverage2.Name = "txtAverage2"
+        Me.txtAverage2.Size = New System.Drawing.Size(60, 22)
+        Me.txtAverage2.TabIndex = 12
+        '
+        'txtTest2c
+        '
+        Me.txtTest2c.Location = New System.Drawing.Point(331, 149)
+        Me.txtTest2c.Name = "txtTest2c"
+        Me.txtTest2c.Size = New System.Drawing.Size(60, 22)
+        Me.txtTest2c.TabIndex = 11
+        '
+        'txtTest1c
+        '
+        Me.txtTest1c.Location = New System.Drawing.Point(331, 103)
+        Me.txtTest1c.Name = "txtTest1c"
+        Me.txtTest1c.Size = New System.Drawing.Size(60, 22)
+        Me.txtTest1c.TabIndex = 6
+        '
+        'txtTest3a
+        '
+        Me.txtTest3a.Location = New System.Drawing.Point(178, 192)
+        Me.txtTest3a.Name = "txtTest3a"
+        Me.txtTest3a.Size = New System.Drawing.Size(60, 22)
+        Me.txtTest3a.TabIndex = 14
+        '
+        'txtTest3b
+        '
+        Me.txtTest3b.Location = New System.Drawing.Point(253, 192)
+        Me.txtTest3b.Name = "txtTest3b"
+        Me.txtTest3b.Size = New System.Drawing.Size(60, 22)
+        Me.txtTest3b.TabIndex = 15
+        '
+        'txtTest2a
+        '
+        Me.txtTest2a.Location = New System.Drawing.Point(178, 149)
+        Me.txtTest2a.Name = "txtTest2a"
+        Me.txtTest2a.Size = New System.Drawing.Size(60, 22)
+        Me.txtTest2a.TabIndex = 9
+        '
+        'txtTest2b
+        '
+        Me.txtTest2b.Location = New System.Drawing.Point(253, 149)
+        Me.txtTest2b.Name = "txtTest2b"
+        Me.txtTest2b.Size = New System.Drawing.Size(60, 22)
+        Me.txtTest2b.TabIndex = 10
+        '
+        'txtAverage1
+        '
+        Me.txtAverage1.Location = New System.Drawing.Point(432, 103)
+        Me.txtAverage1.Name = "txtAverage1"
+        Me.txtAverage1.Size = New System.Drawing.Size(60, 22)
+        Me.txtAverage1.TabIndex = 7
+        '
+        'txtTest1b
+        '
+        Me.txtTest1b.Location = New System.Drawing.Point(253, 103)
+        Me.txtTest1b.Name = "txtTest1b"
+        Me.txtTest1b.Size = New System.Drawing.Size(60, 22)
+        Me.txtTest1b.TabIndex = 5
+        '
+        'txtTest1a
+        '
+        Me.txtTest1a.Location = New System.Drawing.Point(178, 103)
+        Me.txtTest1a.Name = "txtTest1a"
+        Me.txtTest1a.Size = New System.Drawing.Size(60, 22)
+        Me.txtTest1a.TabIndex = 4
+        '
+        'txtName3
+        '
+        Me.txtName3.Location = New System.Drawing.Point(29, 192)
+        Me.txtName3.Name = "txtName3"
+        Me.txtName3.Size = New System.Drawing.Size(125, 22)
+        Me.txtName3.TabIndex = 13
+        '
+        'txtName2
+        '
+        Me.txtName2.Location = New System.Drawing.Point(29, 149)
+        Me.txtName2.Name = "txtName2"
+        Me.txtName2.Size = New System.Drawing.Size(125, 22)
+        Me.txtName2.TabIndex = 8
+        '
+        'txtName1
+        '
+        Me.txtName1.Location = New System.Drawing.Point(29, 103)
+        Me.txtName1.Name = "txtName1"
+        Me.txtName1.Size = New System.Drawing.Size(125, 22)
+        Me.txtName1.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(431, 59)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Average"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(238, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Text Scores"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(62, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'mnuMenu
+        '
+        Me.mnuMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile, Me.mnuReport, Me.mnuAbout})
+        Me.mnuMenu.Location = New System.Drawing.Point(0, 0)
+        Me.mnuMenu.Name = "mnuMenu"
+        Me.mnuMenu.Size = New System.Drawing.Size(579, 28)
+        Me.mnuMenu.TabIndex = 0
+        Me.mnuMenu.Text = "MenuStrip1"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.msReload, Me.msSave, Me.msExit})
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(44, 24)
+        Me.mnuFile.Text = "File"
+        '
+        'msReload
+        '
+        Me.msReload.Name = "msReload"
+        Me.msReload.Size = New System.Drawing.Size(152, 24)
+        Me.msReload.Text = "Reload"
+        '
+        'msSave
+        '
+        Me.msSave.Name = "msSave"
+        Me.msSave.Size = New System.Drawing.Size(152, 24)
+        Me.msSave.Text = "Save"
+        '
+        'msExit
+        '
+        Me.msExit.Name = "msExit"
+        Me.msExit.Size = New System.Drawing.Size(152, 24)
+        Me.msExit.Text = "Exit"
+        '
+        'mnuReport
+        '
+        Me.mnuReport.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.msCalculateAverage})
+        Me.mnuReport.Name = "mnuReport"
+        Me.mnuReport.Size = New System.Drawing.Size(66, 24)
+        Me.mnuReport.Text = "Report"
+        '
+        'mnuAbout
+        '
+        Me.mnuAbout.Name = "mnuAbout"
+        Me.mnuAbout.Size = New System.Drawing.Size(62, 24)
+        Me.mnuAbout.Text = "About"
+        '
+        'msCalculateAverage
+        '
+        Me.msCalculateAverage.Name = "msCalculateAverage"
+        Me.msCalculateAverage.Size = New System.Drawing.Size(198, 24)
+        Me.msCalculateAverage.Text = "Calculate Average"
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(222, 244)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(122, 49)
+        Me.btnClear.TabIndex = 19
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(381, 244)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(122, 49)
+        Me.btnExit.TabIndex = 20
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'FMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(579, 392)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.mnuMenu)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MainMenuStrip = Me.mnuMenu
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Student Test Scores"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.mnuMenu.ResumeLayout(False)
+        Me.mnuMenu.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents mnuMenu As System.Windows.Forms.MenuStrip
+    Friend WithEvents mnuFile As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents msReload As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents msSave As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents msExit As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuReport As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuAbout As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents txtTest3c As System.Windows.Forms.TextBox
+    Friend WithEvents txtAverage3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAverage2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtTest2c As System.Windows.Forms.TextBox
+    Friend WithEvents txtTest1c As System.Windows.Forms.TextBox
+    Friend WithEvents txtTest3a As System.Windows.Forms.TextBox
+    Friend WithEvents txtTest3b As System.Windows.Forms.TextBox
+    Friend WithEvents txtTest2a As System.Windows.Forms.TextBox
+    Friend WithEvents txtTest2b As System.Windows.Forms.TextBox
+    Friend WithEvents txtAverage1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtTest1b As System.Windows.Forms.TextBox
+    Friend WithEvents txtTest1a As System.Windows.Forms.TextBox
+    Friend WithEvents txtName3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtName2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtName1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnCalculateAverage As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents msCalculateAverage As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Homework4-StudentTestScores/FMain.vb
@@ -0,0 +1,432 @@
+﻿Imports System.IO
+Imports System.Windows.Forms
+
+
+
+Public Class FMain
+
+
+
+    Public Structure udtStudent
+
+        Dim strFullName As String
+        Dim strTestScore() As String
+        Dim strAverage As String
+
+    End Structure
+
+    Dim f_audtStudents() As udtStudent
+
+
+    Private Sub btnCalculateAverage_Click(sender As Object, e As EventArgs) Handles btnCalculateAverage.Click
+
+        Try
+
+            If IsValidData() = True Then
+
+                Dim decAverage1 As Decimal
+                Dim decAverage2 As Decimal
+                Dim decAverage3 As Decimal
+
+                'Calculate Average
+                decAverage1 = CalcAverage(txtTest1a, txtTest1b, txtTest1c)
+                decAverage2 = CalcAverage(txtTest2a, txtTest2b, txtTest2c)
+                decAverage3 = CalcAverage(txtTest3a, txtTest3b, txtTest3c)
+
+
+                'Print it in the textboxes
+                txtAverage1.Text = decAverage1.ToString
+                txtAverage2.Text = decAverage2.ToString
+                txtAverage3.Text = decAverage3.ToString
+
+
+            End If
+
+
+        Catch ex As Exception
+
+            MessageBox.Show("Error calculating average", _
+                            "Something went wrong", _
+                            MessageBoxButtons.OK, _
+                            MessageBoxIcon.Exclamation)
+
+        End Try
+
+
+    End Sub
+
+
+    Private Function IsValidData() As Boolean
+
+        Dim blnResult As Boolean = True
+
+        'Valid name?
+        If txtName1.Text <> "" Then
+
+            'yes?
+            If txtTest1a.Text = "" Or txtTest1b.Text = "" Or txtTest1c.Text = "" Then
+
+                'Tests Empty? yes
+                MessageBox.Show(" Test Score for First Student must not empty")
+                blnResult = False
+
+            ElseIf IsNumeric(txtTest1a.Text) = False Then
+
+                'Numeric?
+                MessageBox.Show("Test 1 must be numeric")
+                txtTest1a.Focus()
+                blnResult = False
+
+            ElseIf IsNumeric(txtTest1b.Text) = False Then
+
+                'Numeric?
+                MessageBox.Show("Test 2 must be numeric")
+                txtTest1b.Focus()
+                blnResult = False
+
+            ElseIf IsNumeric(txtTest1c.Text) = False Then
+
+                'Numeric?
+                MessageBox.Show("Test 3 must be numeric")
+                txtTest1c.Focus()
+                blnResult = False
+
+            End If
+
+        ElseIf txtName1.Text = "" Then
+
+            'Name empty? Yes
+            MessageBox.Show("Student 1 cannot be empty", _
+                            "Please fill the textbox")
+            txtName1.Focus()
+            blnResult = False
+
+        End If
+
+        If txtName2.Text <> "" Then
+
+            If txtTest2a.Text = "" Or txtTest2b.Text = "" Or txtTest2c.Text = "" Then
+
+                MessageBox.Show(" Test Score for Second Student must not empty")
+                blnResult = False
+
+            ElseIf IsNumeric(txtTest2a.Text) = False Then
+
+                MessageBox.Show("Test 1 must be numeric")
+                txtTest2a.Focus()
+                blnResult = False
+
+            ElseIf IsNumeric(txtTest2b.Text) = False Then
+
+                MessageBox.Show("Test 2 must be numeric")
+                txtTest2b.Focus()
+                blnResult = False
+
+            ElseIf IsNumeric(txtTest2c.Text) = False Then
+
+                MessageBox.Show("Test 3 must be numeric")
+                txtTest2c.Focus()
+                blnResult = False
+
+            End If
+
+
+        ElseIf txtName2.Text = "" Then
+
+            MessageBox.Show("Student 2 cannot be empty", _
+                            "Please fill the textbox")
+            txtName2.Focus()
+            blnResult = False
+
+        End If
+
+        If txtName3.Text <> "" Then
+
+            If txtTest3a.Text = "" Or txtTest3b.Text = "" Or txtTest3c.Text = "" Then
+
+                MessageBox.Show(" Test Score for Third Student must not empty")
+                blnResult = False
+
+            ElseIf IsNumeric(txtTest3a.Text) = False Then
+
+                MessageBox.Show("Test 1 must be numeric")
+                txtTest3a.Focus()
+                blnResult = False
+
+            ElseIf IsNumeric(txtTest3b.Text) = False Then
+
+                MessageBox.Show("Test 2 must be numeric")
+                txtTest3b.Focus()
+                blnResult = False
+
+            ElseIf IsNumeric(txtTest3c.Text) = False Then
+
+                MessageBox.Show("Test 3 must be numeric")
+                txtTest3c.Focus()
+                blnResult = False
+
+            End If
+
+        ElseIf txtName3.Text = "" Then
+
+            MessageBox.Show("Student 3 cannot be empty", _
+                            "Please fill the textbox")
+            txtName3.Focus()
+            blnResult = False
+
+        End If
+
+        Return blnResult
+
+    End Function
+
+
+
+    Private Function CalcAverage(ByVal txtScore1 As TextBox, ByVal txtScore2 As TextBox, ByVal txtScore3 As TextBox) As Decimal
+
+        Dim decValue As Decimal
+
+        Dim decscore1 As Decimal
+        Dim decScore2 As Decimal
+        Dim decScore3 As Decimal
+
+        decscore1 = Convert.ToDecimal(txtScore1.Text)
+        decScore2 = Convert.ToDecimal(txtScore2.Text)
+        decScore3 = Convert.ToDecimal(txtScore3.Text)
+
+        decValue = decscore1 + decScore2 + decScore3
+
+        decValue = decValue / 3
+
+        Return decValue
+
+    End Function
+
+
+
+
+
+
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        txtName1.Text = ""
+        txtName2.Text = ""
+        txtName3.Text = ""
+
+        txtTest1a.Text = ""
+        txtTest1b.Text = ""
+        txtTest1c.Text = ""
+
+        txtTest2a.Text = ""
+        txtTest2b.Text = ""
+        txtTest2c.Text = ""
+
+        txtTest3a.Text = ""
+        txtTest3b.Text = ""
+        txtTest3c.Text = ""
+
+        txtAverage1.Text = ""
+        txtAverage2.Text = ""
+        txtAverage3.Text = ""
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub FMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        txtAverage1.Enabled = False
+        txtAverage2.Enabled = False
+        txtAverage3.Enabled = False
+
+
+    End Sub
+
+    Private Sub msCalculateAverage_Click(sender As Object, e As EventArgs) Handles msCalculateAverage.Click
+
+        Try
+
+            If IsValidData() = True Then
+
+                Dim decAverage1 As Decimal
+                Dim decAverage2 As Decimal
+                Dim decAverage3 As Decimal
+
+                'Calculate Average
+                decAverage1 = CalcAverage(txtTest1a, txtTest1b, txtTest1c)
+                decAverage2 = CalcAverage(txtTest2a, txtTest2b, txtTest2c)
+                decAverage3 = CalcAverage(txtTest3a, txtTest3b, txtTest3c)
+
+
+                'Print it in the textboxes
+                txtAverage1.Text = decAverage1.ToString
+                txtAverage2.Text = decAverage2.ToString
+                txtAverage3.Text = decAverage3.ToString
+
+
+            End If
+
+
+        Catch ex As Exception
+
+            MessageBox.Show("Error calculating average", _
+                            "Something went wrong", _
+                            MessageBoxButtons.OK, _
+                            MessageBoxIcon.Exclamation)
+
+        End Try
+
+
+    End Sub
+
+
+    Private Sub msSave_Click(sender As Object, e As EventArgs) Handles msSave.Click
+
+        SaveStudentInformation()
+
+
+    End Sub
+
+    Private Sub SaveStudentInformation()
+
+
+        ' Dim ofdStudentFile As New OpenFileDialog
+        ' Dim strInputFile As StreamReader
+        '  Dim 
+
+        Try
+
+            ' ofdStudentFile = System.IO.File.AppendText(strInputFile)
+
+            'obtain Data
+
+
+
+        Catch ex As Exception
+
+        End Try
+
+
+
+
+    End Sub
+
+
+    Private Sub msReload_Click(sender As Object, e As EventArgs) Handles msReload.Click
+
+        LoadStudentFromFile()
+
+
+    End Sub
+
+
+
+    Private Sub LoadStudentFromFile()
+
+
+
+        Dim ofdStudentsFile As New OpenFileDialog
+        Dim strInputFile As StreamReader
+        Dim strStudent As String
+        Dim intIndex As Integer
+
+        'Set Open file parameters
+        ofdStudentsFile.CheckPathExists = True
+        ofdStudentsFile.CheckFileExists = True
+        ofdStudentsFile.Filter = "Text Files (*.txt)| *.txt|All Files (*.*)|*.*"
+
+        'Get the name from the user
+        ofdStudentsFile.ShowDialog()
+
+        If ofdStudentsFile.FileName <> "" Then
+
+            'Yes, so open the file
+            strInputFile = New StreamReader(ofdStudentsFile.FileName)
+
+            'Loop until end of file (Peek= -1)
+            While strInputFile.Peek > -1
+
+                'Read a line
+                strStudent = strInputFile.ReadLine
+
+                'Add it to array
+                AddStudentToStructure(strStudent, intIndex)
+
+                'Next contact
+                intIndex += 1
+
+            End While
+
+        End If
+
+        LoadTextBoxesWithArray()
+
+     
+
+    End Sub
+
+
+    Private Sub AddStudentToStructure(strRecord As String, intRecordIndex As Integer)
+
+
+
+        Dim astrStudentFields() As String
+
+        'Split it
+
+
+        If Not String.IsNullOrWhiteSpace(strRecord) Then
+
+            astrStudentFields = Split(strRecord, ",")
+
+            f_audtStudents(intRecordIndex).strFullName = astrStudentFields(0)
+
+            f_audtStudents(intRecordIndex).strTestScore(0) = astrStudentFields(1)
+
+            f_audtStudents(intRecordIndex).strTestScore(1) = astrStudentFields(2)
+
+            f_audtStudents(intRecordIndex).strTestScore(2) = astrStudentFields(3)
+
+            f_audtStudents(intRecordIndex).strAverage = astrStudentFields(4)
+
+
+        End If
+
+    End Sub
+
+
+
+    Private Sub LoadTextBoxesWithArray()
+
+
+
+        txtName1.Text = f_audtStudents(0).strFullName
+        txtTest1a.Text = f_audtStudents(0).strTestScore(0).ToString
+        txtTest1b.Text = f_audtStudents(0).strTestScore(1).ToString
+        txtTest1c.Text = f_audtStudents(0).strTestScore(2).ToString
+        txtAverage1.Text = f_audtStudents(0).strAverage.ToString
+
+        txtName2.Text = f_audtStudents(1).strFullName
+        txtTest2a.Text = f_audtStudents(1).strTestScore(0).ToString
+        txtTest2b.Text = f_audtStudents(1).strTestScore(1).ToString
+        txtTest2c.Text = f_audtStudents(1).strTestScore(2).ToString
+        txtAverage2.Text = f_audtStudents(1).strAverage.ToString
+
+        txtName3.Text = f_audtStudents(2).strFullName
+        txtTest3a.Text = f_audtStudents(2).strTestScore(0).ToString
+        txtTest3b.Text = f_audtStudents(2).strTestScore(1).ToString
+        txtTest3c.Text = f_audtStudents(2).strTestScore(2).ToString
+        txtAverage3.Text = f_audtStudents(2).strAverage.ToString
+
+
+
+    End Sub
+
+
+
+End Class
